4d8c58f542dfdde939d60de8c14edd66d8bfc5af
ajkreamer <ajkreamer@gmail.com>
2017-11-06 08:30:11 -0500

    Add files via upload

---
+++ bindLinks.js
@@ -0,0 +1,31 @@
+//Function to bind events to the left and right links on the dashboard
+function bindLinks() {
+	//console.log("Removing existing bindings...");
+	
+	//Define jQuery object being acted upon
+	var event,
+		object = $(".activator");
+	
+	//Remove existing bindings
+	object.off();
+	
+	//console.log("Re-binding links...");
+	
+	//Define counter
+	var i = 0;
+	//Cycle through each link
+	while (i < object.length) {
+		if (
+			(object,eq(i).attr("data-activates") != undefined)
+			&& (object,eq(i).attr("data-activates") != "")
+		) event = (object,eq(i).attr("data-activates");
+		else event = "click";
+		
+		//Bind the button to the function described in the data-activates attribute
+		object.eq(i).on(event, eval(object.eq(i).attr("data-activates")));
+		
+		i++;
+	}
+	
+	//console.log("Link-binding function complete!");
+}
