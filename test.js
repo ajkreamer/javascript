a2bf015daa89baccf0463038d4cd66c35e9a1e4e
ajkreamer <ajkreamer@gmail.com>
2017-11-20 09:40:35 -0500

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-//Function to test certain functions
-function test() {
-	//Spit out message in console
-	console.log("Testing");
-	
-	//Spit out materialize toast message
-	toastShowMessage("none", "Testing");
-}
